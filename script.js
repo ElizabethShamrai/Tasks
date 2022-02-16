3df67151fb92fc1783dcc2bb622aa605deb9253b
Elizabeth <96345335+ElizabethShamrai@users.noreply.github.com>
2022-02-16 19:08:48 +0200

    task

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+let name = 'Джон';
+let admin;
+admin = name;
+alert(admin);
+//----------------
+let nameOurPlanet = 'Globe';
+let actualUserName = 'John';
+//----------------
+let num = '10' + 10;
+console.log(typeof(num))
